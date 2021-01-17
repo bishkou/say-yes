3ab47f369dbcfd32b15ea9066ea2ea227dbb7d26
bishkou <chedyhammami@yahoo.com>
2021-01-17 20:42:15 +0100

    log route

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -4,11 +4,16 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', (req,res) => {
-    res.json({
-        message: 'gggggg'
-    })
+router.get('/', async (req,res, next) => {
+    try {
+        const entries = await LogEntry.find();
+        res.json(entries)
+    } catch (error) {
+        next(error)
+    }
+
 })
+
 
 router.post('/',async (req, res, next) => {
     try {
@@ -17,6 +22,9 @@ router.post('/',async (req, res, next) => {
         res.json(createdEntry);
     }
     catch (error) {
+        if (error.name === 'ValidationError') {
+            res.status(422);
+        }
         next(error)
     }
 })
